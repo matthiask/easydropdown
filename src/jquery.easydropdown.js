bdc7d95ac32052abd46e15326ed50a29ac747b88
Simon Baechler <sb@feinheit.ch>
2015-11-04 13:33:30 +0100

    fix select by value (see PR 69 and 70)

--- src/jquery.easydropdown.js
+++ src/jquery.easydropdown.js
@@ -318,7 +318,7 @@
 			var self = this;
 
 			if(typeof index === 'string'){
-				index = self.$select.find('option[value='+index+']').index() - 1;
+				index = self.$select.find('option[value='+index+']').index();
 			};
 
 			var	option = self.options[index],
